23c010a7cf35ba33236f0066a0a178a08e31b998
Raul Silva <rraulsilva.dev@gmail.com>
2025-08-20 11:50:54 -0300

    feat(setup): deploy workflow

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -2,12 +2,12 @@ import { JwtModuleOptions } from '@nestjs/jwt';
 import 'dotenv/config';
 
 const EXPIREIN = Number(process.env?.JWT_EXPIREIN ?? 60);
-export const JWT_SECRECT = process.env.JWT_SECRECT;
+export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_REFRESH_TOKEN_SECRECT = process.env.JWT_REFRESH_TOKEN_SECRECT;
 export const JWT_EXPIREIN = `${EXPIREIN}s`;
 export const JWT_REFRESH_TOKEN_EXPIREIN = `${EXPIREIN * 2}s`;
 export const jwtConfig: JwtModuleOptions = {
   global: true,
-  secret: JWT_SECRECT,
+  secret: JWT_SECRET,
   signOptions: { expiresIn: JWT_EXPIREIN },
 };
